f13b7aef068c4d6f36bf644009f712837b83f0e6
backendStark <239644803+backendStark@users.noreply.github.com>
2025-10-23 00:00:14 +0500

    add load tasks function

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -11,11 +13,50 @@ type Task struct {
 	Done bool
 }
 
+var todosFilename string = "todos.json"
+
+func loadTasks(filename string) ([]Task, error) {
+	if data, err := os.ReadFile(filename); err == nil {
+		if len(data) == 0 {
+			return []Task{}, nil
+		}
+
+		var tasks []Task
+
+		if err := json.Unmarshal(data, &tasks); err != nil {
+			return nil, fmt.Errorf("error parsing json %w", err)
+		}
+
+		return tasks, nil
+	} else if errors.Is(err, os.ErrNotExist) {
+		// file, err := os.Create(filename)
+
+		// if err != nil {
+		// 	return nil, fmt.Errorf("error creating json file %w", err)
+		// }
+
+		// defer file.Close()
+
+		return []Task{}, nil
+	} else {
+		return nil, fmt.Errorf("file has errors %w", err)
+	}
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("If you want use program, you need to input arguments. Try again")
 		return
 	}
+
+	tasks, err := loadTasks(todosFilename)
+
+	if err != nil {
+		fmt.Println("error with parsing tasks file:", err)
+		return
+	}
+
+	fmt.Println(tasks)
 
 	act := os.Args[1]
 
