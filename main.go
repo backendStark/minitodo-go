c2a2fc726abbbd94954e15480fe4d238cb0a18cd
backendStark <239644803+backendStark@users.noreply.github.com>
2025-10-22 23:12:28 +0500

    made base of programm

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
-package minitodo
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type Task struct {
+	text string
+	done bool
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("If you want use program, you need to input arguments. Try again")
+		return
+	}
+
+	act := os.Args[1]
+
+	switch act {
+	case "add":
+		fmt.Println("Add task")
+	case "list":
+		fmt.Println("Show list")
+	case "done":
+		fmt.Println("Make the task done")
+	default:
+		fmt.Println("Unknown command")
+	}
+}
