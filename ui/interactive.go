f2dcc6634647a56927dc054b1e97951b34b5d1a0
backendStark <239644803+backendStark@users.noreply.github.com>
2025-10-25 21:01:21 +0500

    start making Update method

--- ui/interactive.go
+++ ui/interactive.go
@@ -1,15 +1,34 @@
 package ui
 
 import (
-	"github.com/charmbracelet/bubbletea"
+	"fmt"
 	"minitodo/models"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
+
 type model struct {
-	cursor int
-	tasks []models.Task
+	cursor     int
+	tasks      []models.Task
 	pathToFile string
 }
 
 func (m model) Init() tea.Cmd {
 	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "up":
+			fmt.Println("Переводим лист вверх (уменьшим cursor на 1)")
+		case "down":
+			fmt.Println("Переводим лист вниз (увеличим cursor на 1)")
+		case " ":
+			fmt.Println("Toggle для чеклиста")
+		case "esc":
+			fmt.Println("Выходим из режима list")
+		}
+	}
 }
