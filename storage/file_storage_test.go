5851eb006ed04b94a07ac0147405b89e0958aa31
backendStark <239644803+backendStark@users.noreply.github.com>
2025-11-01 11:30:46 +0500

    add test for correct load if file not exist

--- storage/file_storage_test.go
+++ storage/file_storage_test.go
@@ -29,3 +29,35 @@ func TestNewFileStorage_Success(t *testing.T) {
 		t.Errorf("Expect FileStorage filename equal %s, but got: %s", filename, fs.filename)
 	}
 }
+
+func TestFileStorage_Load_FileNotExist(t *testing.T) {
+	filename := filepath.Join(t.TempDir(), "not_existing_file.json")
+	fs := NewFileStorage(filename)
+
+	if fs == nil {
+		t.Fatal("Expected FileStorage to be non-nil, got nil")
+	}
+
+	tasks, err := fs.Load()
+	if err != nil {
+		t.Error("Error loading tasks")
+	}
+
+	if len(tasks) != 0 {
+		t.Error("Expected not exisiting file creating with empty tasks list")
+	}
+
+	if _, err := os.Stat(filename); err != nil {
+		t.Error("Expected Stat method return FileInfo")
+	}
+
+	data, err := os.ReadFile(filename)
+
+	if err != nil {
+		t.Error("Error reading file:", filename)
+	}
+
+	if string(data) != "[]" {
+		t.Error("Expected data equal '[]', but got:", string(data))
+	}
+}
