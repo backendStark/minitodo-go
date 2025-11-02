95d0d034fd16cf902119d888604034a2cb78afd3
backendStark <239644803+backendStark@users.noreply.github.com>
2025-11-02 17:02:42 +0500

    add enum for Sort mode and method String

--- models/task.go
+++ models/task.go
@@ -8,3 +8,21 @@ type Task struct {
 func (t *Task) Toggle() {
 	t.Done = !t.Done
 }
+
+type SortMode int
+
+const (
+	SortByStatus SortMode = iota
+	SortByStatusReverse
+)
+
+func (sm SortMode) String() string {
+	switch sm {
+	case SortByStatus:
+		return "↑Active first"
+	case SortByStatusReverse:
+		return "↓Done first"
+	default:
+		return "Unknown"
+	}
+}
